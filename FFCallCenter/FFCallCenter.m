41a2b4971a55087cc539fe1149ffa722a96c1a1a
shaggon <shaggon.du@farfetch.com>
2017-12-07 23:06:50 +0800

    0.0.6

--- FFCallCenter/FFCallCenter.m
+++ FFCallCenter/FFCallCenter.m
@@ -28,41 +28,6 @@
     return callCenter;
 }
 
-/*
- scheme://[target]/[action]?[params]
- 
- url sample:
- aaa://targetA/actionB?id=1234
- */
-
-- (id)performActionWithUrl:(NSURL *)url completion:(void (^)(NSDictionary *))completion
-{
-    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
-    NSString *urlString = [url query];
-    for (NSString *param in [urlString componentsSeparatedByString:@"&"]) {
-        NSArray *elts = [param componentsSeparatedByString:@"="];
-        if([elts count] < 2) continue;
-        [params setObject:[elts lastObject] forKey:[elts firstObject]];
-    }
-    
-    // 这里这么写主要是出于安全考虑，防止黑客通过远程方式调用本地模块。这里的做法足以应对绝大多数场景，如果要求更加严苛，也可以做更加复杂的安全逻辑。
-    NSString *actionName = [url.path stringByReplacingOccurrencesOfString:@"/" withString:@""];
-    if ([actionName hasPrefix:@"native"]) {
-        return @(NO);
-    }
-    
-    // 这个demo针对URL的路由处理非常简单，就只是取对应的target名字和method名字，但这已经足以应对绝大部份需求。如果需要拓展，可以在这个方法调用之前加入完整的路由逻辑
-    id result = [self performTarget:url.host action:actionName params:params shouldCacheTarget:NO];
-    if (completion) {
-        if (result) {
-            completion(@{@"result":result});
-        } else {
-            completion(nil);
-        }
-    }
-    return result;
-}
-
 - (id)performTarget:(NSString *)targetName action:(NSString *)actionName params:(NSDictionary *)params shouldCacheTarget:(BOOL)shouldCacheTarget
 {
     
@@ -79,8 +44,8 @@
     SEL action = NSSelectorFromString(actionString);
     
     if (target == nil) {
-        // 这里是处理无响应请求的地方之一，这个demo做得比较简单，如果没有可以响应的target，就直接return了。实际开发过程中是可以事先给一个固定的target专门用于在这个时候顶上，然后处理这种请求的
-        NSLog(@"💥💥💥💥💥💥💥找不到 %@💥💥💥💥💥💥💥",targetClassString);
+        // if you can't find the target. This is the place you can handle it.
+        NSLog(@"💥💥💥💥💥💥💥can't find %@💥💥💥💥💥💥💥",targetClassString);
         return nil;
     }
     
@@ -91,18 +56,18 @@
     if ([target respondsToSelector:action]) {
         return [self safePerformAction:action target:target params:params];
     } else {
-        // 有可能target是Swift对象
+        // for Swift object
         actionString = [NSString stringWithFormat:@"Action_%@WithParams:", actionName];
         action = NSSelectorFromString(actionString);
         if ([target respondsToSelector:action]) {
             return [self safePerformAction:action target:target params:params];
         } else {
-            // 这里是处理无响应请求的地方，如果无响应，则尝试调用对应target的notFound方法统一处理
+            // if you can't find the action. Try to use the notFound: Action in that target.
             SEL action = NSSelectorFromString(@"notFound:");
             if ([target respondsToSelector:action]) {
                 return [self safePerformAction:action target:target params:params];
             } else {
-                // 这里也是处理无响应请求的地方，在notFound都没有的时候，这个demo是直接return了。实际开发过程中，可以用前面提到的固定的target顶上的。
+                // if you even can't find notFound:. This is the place to handle it.
                 [self.cachedTarget removeObjectForKey:targetClassString];
                 return nil;
             }
@@ -119,10 +84,8 @@
     }
     const char* retType = [methodSig methodReturnType];
     
-    //返回值为void的情况
     if (strcmp(retType, @encode(void)) == 0) {
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
-        // 参数必须从第2个索引开始，因为前两个已经被target和selector使用
         [invocation setArgument:&params atIndex:2];
         [invocation setSelector:action];
         [invocation setTarget:target];
@@ -130,7 +93,6 @@
         return nil;
     }
     
-    //返回值为NSInteger的情况
     if (strcmp(retType, @encode(NSInteger)) == 0) {
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
         [invocation setArgument:&params atIndex:2];
@@ -142,7 +104,6 @@
         return @(result);
     }
     
-    //返回值为BOOL的情况
     if (strcmp(retType, @encode(BOOL)) == 0) {
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
         [invocation setArgument:&params atIndex:2];
@@ -154,7 +115,6 @@
         return @(result);
     }
     
-    //返回值为CGFloat的情况
     if (strcmp(retType, @encode(CGFloat)) == 0) {
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
         [invocation setArgument:&params atIndex:2];
@@ -166,7 +126,6 @@
         return @(result);
     }
     
-    //返回值为NSUInteger的情况
     if (strcmp(retType, @encode(NSUInteger)) == 0) {
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
         [invocation setArgument:&params atIndex:2];
